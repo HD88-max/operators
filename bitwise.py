4d9c4a02875f6c3fbdc05c67f4668fbf67a10a23
HD88-max <hardikdhallam@gmail.com>
2024-12-27 10:42:56 +0000

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,6 @@
+a = 6
+b = 8
+print(a<<b)
+c = 1000
+d = 3
+print(c>>d)
---
+++ is_not.py
@@ -0,0 +1,13 @@
+x=20
+y=20
+if x is not y:
+    print("Not equal to each other.")
+else:
+    print("Equal to each other")
+
+x = int(input("Enter a number 1 - 10: "))
+y = 8
+if x == y:
+    print("Your number is equal to {0}".format(y))
+elif x != y:
+    print("Your number is not equal to {0}".format(y))
